828af3bfcb818631589a768fe054ed4bea356de0
Emily <emelizadaniels@gmail.com>
2022-10-17 15:44:06 -0400

    commit tree 63fefbfb368716ab91b3631a2d122b1c41387a3e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
+   const inputForm = document.querySelector('form');
+   
+   inputForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const input = document.querySelector('input#searchByID');
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+       const title = document.querySelector('section#movieDetails h4');
+       const summary = document.querySelector('section#movieDetails p');
+
+       title.innerText = data.title;
+       summary.innerText = data.summary;
+      
+        });
+    });
   
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+// Your primary task will be to get this form working. When a user inputs a valid ID, the movie information should appear on the page.
+
+// Doing this will involve a few steps:
+
+// Add event listeners to capture form data and override a form's default behavior
+// Fetch data based on what the user types into that form
+// Display that data on the page
